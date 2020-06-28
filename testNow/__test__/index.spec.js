d36c977dc15c9f0d54d732bf2f9acd6f24066300
冯婷婷 <fengtingting6210@gmail.com>
2020-06-28 08:23:40 +0800

    feature:测试代码生成

--- testNow/__test__/index.spec.js
+++ testNow/__test__/index.spec.js
@@ -1,8 +1,25 @@
-test('测试文件名生成',() => {
-  //因为是引入一个class类，所以要进行实例化，然后令方法立即执行
-  const src=new(require('../index'))()
-  const ret=src.getTestFileName('/abc/class.js');
-  //我们期待一个方法，令我们的测试用例在测试某个文件时，若文件名是/abc/class.js则生成文件名为/abc/__test__/class.spec.js
+test('测试生成代码' , () => {
+  //第一步，引入文件，进行实例化
+  const src = new (require('../index'))
+  //第二步，调用源文件中的方法，按照要求传入参数
+  const ret=src.getTestSource('fun','class')
+  //第三步，打印结果
   expect(ret)
-    .toBe('/abc/__test__/class.spec.js')
+    .toBe(`
+test('Test fun', () => {
+const fun = require('../class')
+const ret = fun()
+// expect(ret)
+//  toBe('test return')
 })
+    `)
+})
+
+// test('测试文件名生成',() => {
+//   //因为是引入一个class类，所以要进行实例化，然后令方法立即执行
+//   const src=new(require('../index'))()
+//   const ret=src.getTestFileName('/abc/class.js');
+//   //我们期待一个方法，令我们的测试用例在测试某个文件时，若文件名是/abc/class.js则生成文件名为/abc/__test__/class.spec.js
+//   expect(ret)
+//     .toBe('/abc/__test__/class.spec.js')
+// })
--- testNow/index.js
+++ testNow/index.js
@@ -1,6 +1,28 @@
 const path=require('path');
 module.exports = class TestNow{
   /**
+   * 测试代码生成
+   * @param {methodName,classFile,isClass}
+  */
+  getTestSource(methodName, classFile, isClass = false){
+    //第一步：打印方法名称
+    console.log('getTestSource',methodName)
+    //第二步：返回测试用例
+    return `
+test('${'Test '+ methodName }', () => {
+const ${isClass?'{'+ methodName +'}' : methodName} = require('${'../'+classFile}')
+const ret = ${methodName}()
+// expect(ret)
+//  toBe('test return')
+})
+    `
+  }
+
+
+
+
+
+  /**
    * 生成测试文件名
    * @param {*} filename 代码文件名
   */
